4a0e600da0007accd4265f6740dcb1053d614ec4
datvu1907 <46070070+datvu1907@users.noreply.github.com>
2019-12-05 22:10:07 +0700

    data dien thoai

---
+++ data/Data.js
@@ -0,0 +1,111 @@
+
+var Iphone11 =  {
+    key: 1,
+    title: "Iphone 11",
+    price: "20.000.000 VNĐ",
+    desciption: "Màn hình:	IPS LCD, 6.1 inch Liquid Retina\n Hệ điều hành:	iOS 13\n Camera sau:	Chính 12 MP & Phụ 12 MP\n Camera trước:	12 MP\n CPU:	Apple A13 Bionic 6 nhân\n RAM:	4 GB\n Bộ nhớ trong:	64,256,512 GB\n Thẻ SIM:   1 eSIM & 1 Nano SIM, Hỗ trợ 4G\n Dung lượng pin:	3110 mAh, có sạc nhanh",
+    img1: "\image\Ip 11_den.png",
+    img2: "\image\Ip 11_tim.png",
+    img3: "\image\Ip 11_trang.png",
+    img4: "\image\Ip 11_xanh.png"
+};
+
+var Iphone11pro =  {
+    key: 2,
+    title: "Iphone 11 pro",
+    price: "30.990.000 VNĐ",
+    desciption: "Màn hình:	OLED, 5.8 inch, Super Retina XDR\n Hệ điều hành:	iOS 13\n Camera sau:	3 camera 12 MP\n Camera trước:	12 MP\n CPU:	Apple A13 Bionic 6 nhân\n RAM:	4 GB\n Bộ nhớ trong:	64,256,512 GB\n Thẻ SIM:   1 eSIM & 1 Nano SIM, Hỗ trợ 4G\n Dung lượng pin:	3046 mAh, có sạc nhanh",
+    img1: "\image\Ip 11pro_den.png",
+    img2: "\image\Ip 11pro_trang.png",
+    img3: "\image\Ip 11pro_vang.png",
+    img4: "\image\Ip 11pro_xanh.png"
+};
+
+var Iphone11promax =  {
+    key: 3,
+    title: "Iphone 11 pro max",
+    price: "33.990.000 VNĐ",
+    desciption: "Màn hình:	OLED, 6.5 inch, Super Retina XDR\n Hệ điều hành:	iOS 13\n Camera sau:	3 camera 12 MP\n Camera trước:	12 MP\n CPU:	Apple A13 Bionic 6 nhân\n RAM:	4 GB\n Bộ nhớ trong:	64,256,512 GB\n Thẻ SIM:   1 eSIM & 1 Nano SIM, Hỗ trợ 4G\n Dung lượng pin:	3046 mAh, có sạc nhanh",
+    img1: "\image\Ip 11promax_den.png",
+    img2: "\image\Ip 11promax_trang.png",
+    img3: "\image\Ip 11promax_vang.png",
+    img4: "\image\Ip 11promax_xanh.png"
+};
+
+var SSgalaxys10 = {
+    key: 4,
+    title:"Samsung Galaxy s10",
+    price: "19.990.000 VNĐ",
+    desciption: "Màn hình:	Dynamic AMOLED, 6.1 inch, Quad HD+ (2K+)\n Hệ điều hành:	Android 9.0 (Pie)\n Camera sau:		Chính 12 MP & Phụ 12 MP, 16 MP\n Camera trước:	10 MP\n CPU:	Exynos 9820 8 nhân\n RAM:	8 GB\n Bộ nhớ trong:	128 GB\n Thẻ SIM:   2 SIM Nano (SIM 2 chung khe thẻ nhớ), Hỗ trợ 4G\n Dung lượng pin:	3400 mAh, có sạc nhanh",
+    img1: "\image\SSgalaxy s10_den.png",
+    img2: "\image\SSgalaxy s10_trang.png",
+    img3: "\image\SSgalaxy s10_xanh.png",
+    img4: "\image\SSgalaxy s10_canh.png"
+};
+
+var SSgalaxys10plus = {
+    key: 5,
+    title: "Samsung Galaxy s10 plus",
+    price: "23.823.000 VNĐ",
+    desciption: "Màn hình:	Dynamic AMOLED, 6.4 inch, Quad HD+ (2K+)\n Hệ điều hành:	Android 9.0 (Pie)\n Camera sau:		Chính 12 MP & Phụ 12 MP, 16 MP\n Camera trước:	Chính 10 MP & Phụ 8 MP\n CPU:	Exynos 9820 8 nhân\n RAM:	8 GB\n Bộ nhớ trong:	128 GB\n Thẻ SIM:   2 SIM Nano (SIM 2 chung khe thẻ nhớ), Hỗ trợ 4G\n Dung lượng pin:	4100 mAh, có sạc nhanh",
+    img1: "\image\SSgalaxy s10plus.png",
+    img2: "\image\SSgalaxy s10plus_den.png",
+    img3: "\image\SSgalaxy s10plus_trang.png",
+    img4: "\image\SSgalaxy s10plus_xanh.png"
+};
+
+var SSgalaxynote10plus = {
+    key: 6,
+    title:"Samsung Galaxy note 10 plus",
+    price: "26.990.000 VNĐ",
+    desciption: "Màn hình:	Dynamic AMOLED, 6.8 inch, Quad HD+ (2K+)\n Hệ điều hành:	Android 9.0 (Pie)\n Camera sau:		Chính 12 MP & Phụ 12 MP, 16 MP, TOF 3D\n Camera trước:	 10 MP\n CPU:	Exynos 9825 8 nhân\n RAM:	12 GB\n Bộ nhớ trong:	256 GB\n Thẻ SIM:   2 SIM Nano (SIM 2 chung khe thẻ nhớ), Hỗ trợ 4G\n Dung lượng pin:	4300 mAh, có sạc nhanh",
+    img1: "\image\SSgalaxy note10plus_den.png",
+    img2: "\image\SSgalaxy note10plus_hong.png",
+    img3: "\image\SSgalaxy note10plus_tim.png",
+    img4: "\image\SSgalaxy note10plus_xanh.png"
+};
+
+var SSgalaxyfold = {
+    key: 7,
+    title:"Samsung Galaxy Fold",
+    price: "50.000.000 VNĐ",
+    desciption: "Màn hình:	Dynamic AMOLED, phụ: Super AMOLED, Chính 7.3 inch & Phụ 4.6 inch, Quad HD (2K)\n Hệ điều hành:	Android 9.0 (Pie)\n Camera sau:		Chính 12 MP & Phụ 12 MP, 16 MP\n Camera trước:	 Trong: 10 MP, 8 MP; Ngoài: 10 MP\n CPU:	Snapdragon 855 8 nhân\n RAM:	12 GB\n Bộ nhớ trong:	512 GB\n Thẻ SIM:   1 eSIM & 1 Nano SIM, Hỗ trợ 4G\n Dung lượng pin:	6000 mAh, có sạc nhanh",
+    img1: "\image\SSgalaxy fold.png",
+    img2: "\image\SSgalaxy fold_truoc.png",
+    img3: "\image\SSgalaxy fold_sau.png"
+};
+
+var Huaweip30pro = {
+    key: 8,
+    title: "Huawei P30 pro",
+    price: "20.990.000 VNĐ",
+    desciption: "Màn hình:	OLED, 6.47 inch, Full HD+\n Hệ điều hành:	Android 9.0 (Pie)\n Camera sau:		Chính 40 MP & Phụ 20 MP, 8 MP, TOF 3D\n Camera trước:	 32 MP\n CPU:	Kirin 980 8 nhân\n RAM:	8 GB\n Bộ nhớ trong:	256 GB\n Thẻ SIM:   2 SIM Nano (SIM 2 chung khe thẻ nhớ), Hỗ trợ 4G\n Dung lượng pin:	4200 mAh, có sạc nhanh",
+    img1: "\image\Huawei p30pro_trang.png",
+    img2: "\image\Huawei p30pro_trang_lung.png",
+    img3: "\image\Huawei p30pro_xanh.png",
+    img4: "\image\Huawei p30pro_xanh_lung.png"
+};
+
+var Opporeno2 = {
+    key : 9,
+    title:"Oppo Reno 2",
+    price: "14.900.000 VNĐ",
+    desciption: "Màn hình:	Sunlight AMOLED, 6.55 inch, Full HD+\n Hệ điều hành:	Android 9.0 (Pie)\n Camera sau:		Chính 48 MP & Phụ 13 MP, 8 MP, 2 MP\n Camera trước:	 16 MP\n CPU:	Snapdragon 730G 8 nhân\n RAM:	8 GB\n Bộ nhớ trong:	256 GB\n Thẻ SIM:   2 SIM Nano (SIM 2 chung khe thẻ nhớ), Hỗ trợ 4G\n Dung lượng pin:	4000 mAh, có sạc nhanh",
+    img1: "\image\Oppo reno2_den.png",
+    img2: "\image\Oppo reno2_vien_den.png",
+    img3: "\image\Oppo reno2_hong.png",
+    img4: "\image\Oppo reno2_vien_hong.png"
+};
+
+var Vivov17pro = {
+    key : 10,
+    title:"Vivo V17 pro",
+    price:" 9.490.000 VNĐ",
+    desciption: "Màn hình:	Super AMOLED, 6.44 inch, Full HD+\n Hệ điều hành:	Android 9.0 (Pie)\n Camera sau:		Chính 48 MP & Phụ 8 MP, 2 MP, 2 MP\n Camera trước:	 Chính 32 MP & Phụ 8 MP\n CPU:	Snapdragon 675 8 nhân 64-bit\n RAM:	8 GB\n Bộ nhớ trong:	128 GB\n Thẻ SIM:   2 Nano SIM, Hỗ trợ 4G\n Dung lượng pin:	4100 mAh, có sạc nhanh",
+    img1: "\image\Vivo v17pro_den.png",
+    img2: "\image\Vivo v17pro_den_canh.png",
+    img3: "\image\Vivo v17pro_trang.png",
+    img4: "\image\Vivo v17pro_trang_canh.png"
+};
+
+var Phone = [Iphone11, Iphone11pro, Iphone11promax, SSgalaxys10, SSgalaxys10plus, SSgalaxynote10plus, SSgalaxyfold, Huaweip30pro, Opporeno2, Vivov17pro];
