eaab18d676f7e46447a9f374ac50da0ddd83ef55
sylvianguyen02 <54926449+sylvianguyen02@users.noreply.github.com>
2019-12-06 07:58:23 +0700

    Update data laptop

--- data/Data.js
+++ data/Data.js
@@ -109,3 +109,116 @@ var Vivov17pro = {
 };
 
 var Phone = [Iphone11, Iphone11pro, Iphone11promax, SSgalaxys10, SSgalaxys10plus, SSgalaxynote10plus, SSgalaxyfold, Huaweip30pro, Opporeno2, Vivov17pro];
+
+
+var DellXPS15 =  {
+    key: 1,
+    title: "Dell XPS15 7590",
+    price: "72.000.000 VNĐ",
+    desciption: "CPU :	Intel, Core i9\n RAM :	32 GB, DDR4\n Ổ cứng :	SSD, 1TB\n Màn hình : 15.6 inchs \n Card màn hình : NVIDIA GeForce GTX 1650, Card rời và tích hợp\n Cổng kết nối : LAN : Không, WIFI : 802.11 ac\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 2 kg\n Kích thước : 1.7x35.7x23.5 cm(HxWxD)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\dell-xps-15-7590-bac-1.png",
+    img2: "\image\dell-xps-15-7590-bac-2.png",
+    img3: "\image\dell-xps-15-7590-bac-3.png",
+    img4: "\image\dell-xps-15-7590-bac-4.png"
+};
+
+var DellXPS13 =  {
+    key: 2,
+    title: "Dell XPS13 7390",
+    price: "54.990.000 VNĐ",
+    desciption: "CPU :	Intel, Core i7\n RAM :	16 GB, LRDDR3\n Ổ cứng : SSD, 512GB\n Màn hình : 13.3 inchs \n Card màn hình :	Intel UHD Graphics 620, Tích hợp, Card rời và tích hợp\n Cổng kết nối :	LAN : 10/100/1000 Mbps, WIFI : Wifi 6 (802.11ax)\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 1.21 kg\n Kích thước : 302 x 199 x 7.8-11.6 mm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\dell-xps13-7390-bac-1.png",
+    img2: "\image\dell-xps13-7390-bac-2.png",
+    img3: "\image\dell-xps13-7390-bac-3.png",
+    img4: "\image\dell-xps13-7390-bac-4.png"
+};
+
+var DellXPS139 =  {
+    key: 3,
+    title: "Dell XPS13 9370",
+    price: "44.990.000 VNĐ",
+    desciption: "CPU :	Intel, Core i7\n RAM :	8 GB, DDR4\n Ổ cứng : SSD, 256GB\n Màn hình : 13.3 inchs \n Card màn hình : Intel UHD Graphics 620, Tích hợp\n Cổng kết nối : LAN : 10/100/1000 Mbps, WIFI : Wifi 6 (802.11ax)\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 1.21 kg\n Kích thước : 302 x 199 x 7.8-11.6 mm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\dell-xps13-9370-i7-vang-2.png",
+    img2: "\image\dell-xps13-9370-i7-vang-2.png",
+    img3: "\image\dell-xps13-9370-i7-vang-2.png",
+    img4: "\image\dell-xps13-9370-i7-vang-2.png"
+};
+
+var DellG3 = {
+    key: 4,
+    title:"Dell G3 N3590",
+    price: "32.990.000 VNĐ",
+    desciption: "CPU :	Intel, Core i7\n RAM :	8 GB, DDR4\n Ổ cứng : SSD, 512GB\n Màn hình : 15.6 inchs \n Card màn hình : NVIDIA GeForce GTX 1660Ti, Card rời\n Cổng kết nối : LAN : 10/100/1000 Mbps, WIFI : Wifi 6 (802.11ax)\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 1.21 kg\n Kích thước : 302 x 199 x 7.8-11.6 mm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\dell-g3-n3590-den-1.png",
+    img2: "\image\dell-g3-n3590-den-2.png",
+    img3: "\image\dell-g3-n3590-den-3.png",
+    img4: "\image\dell-g3-n3590-den-4.png"
+};
+
+var DellInspiron = {
+    key: 5,
+    title: "Dell Inspiron N7391",
+    price: "26.990.000 VNĐ",
+    desciption: "CPU :	Intel, Core i5\n RAM :	8 GB, LRDDR3\n Ổ cứng : SSD, 512GB\n Màn hình : 13.3 inchs \n Card màn hình :	Intel UHD Graphics with shared graphics memory\n Cổng kết nối :	LAN : 10/100/1000 Mbps, WIFI : Wifi 802.11 ac\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 1.45 kg\n Kích thước : 30.67 x 21.6 x 1.59 cm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\dell-inspiron-n7391-den-1.png",
+    img2: "\image\dell-inspiron-n7391-den-2.png",
+    img3: "\image\dell-inspiron-n7391-den-3.png",
+    img4: "\image\dell-inspiron-n7391-den-4.png"
+};
+
+var AsusZenDuo = {
+    key: 6,
+    title:"Asus ZenBook Duo",
+    price: "32.990.000 VNĐ",
+    desciption: "CPU :	Intel, Core i5 Comet Lake\n RAM : 8 GB, LRDDR3\n Ổ cứng : SSD, 512GB\n Màn hình : 14.0 inchs \n Card màn hình :	NVIDIA GeForce MX250, Card rời và tích hợp\n Cổng kết nối :	LAN : Không, WIFI : 802.11 ac\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 1.5 kg\n Kích thước : 323 x 199 x 22.3 cm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\asus-zenbook-duo-ux481-xanh-1.png",
+    img2: "\image\asus-zenbook-duo-ux481-xanh-2.png",
+    img3: "\image\asus-zenbook-duo-ux481-xanh-3.png",
+    img4: "\image\asus-zenbook-duo-ux481-xanh-4.png"
+};
+
+var AsusStrixG531GT = {
+    key: 7,
+    title:"Asus Strix G531GT",
+    price: "28.990.000 VNĐ",
+    desciption: "CPU :	Intel, Core i7\n RAM :	8 GB, DDR4\n Ổ cứng : SSD, 512GB\n Màn hình : 15.6 inchs \n Card màn hình : GTX1650, Card rời\n Cổng kết nối :	LAN : 10/100/1000 Mbps, WIFI : 802.11ac\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 2.4 kg\n Kích thước : 36 x 27 x 2.58 cm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\images\asus-strix-g531gt-den-1.png",
+    img2: "\images\asus-strix-g531gt-den-2.png",
+    img3: "\images\asus-strix-g531gt-den-3.png",
+    img4: "\images\asus-strix-g531gt-den-4.png"
+};
+
+var AsusZenUX = {
+    key: 8,
+    title: "Asus Zenbook UX434FLC",
+    price: "26.490.000 VNĐ",
+    desciption: "CPU :	Intel, Core i5 Comnet Lake\n RAM : 8 GB, LRDDR3\n Ổ cứng : SSD, 512GB\n Màn hình : 14.0 inchs \n Card màn hình : NVIDIA MX250, Card rời và tích hợp\n Cổng kết nối : LAN : không, WIFI : Wifi 6 (802.11ax)\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 1.26 kg\n Kích thước : 31.9 x 1.99 x 1.69 cm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\asus-zenbook-ux434-bac-1.png",
+    img2: "\image\asus-zenbook-ux434-bac-2.png",
+    img3: "\image\asus-zenbook-ux434-bac-3.png",
+    img4: "\image\asus-zenbook-ux434-bac-4.png"
+};
+
+var AsusZephyrus = {
+    key : 9,
+    title:"Asus Zephyrus",
+    price: "31.900.000 VNĐ",
+    desciption: "CPU :	AMD, Ryzen 7\n RAM : 8 GB, DDR4\n Ổ cứng : SSD, 512GB\n Màn hình : 15.6 inchs \n Card màn hình : NVIDIA GeForce GTX1660Ti; Radeon RX Vega 10 Graphics, Card rời và tích hợp\n Cổng kết nối : LAN : 10/100/1000 Mbps, WIFI : 802.11 ac\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 2.104 kg\n Kích thước : 36 x 25.2 x 1.99 cm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\asus-zephyrus-ga502-1.png",
+    img2: "\image\asus-zephyrus-ga502-2.png",
+    img3: "\image\asus-zephyrus-ga502-3.png",
+    img4: "\image\asus-zephyrus-ga502-4.png"
+};
+
+var AsusStrixG531GD = {
+    key : 10,
+    title:"Asus Strix G531GD",
+    price:" 22.990.000 VNĐ",
+    desciption: "CPU :	Intel, Core i5\n RAM : 8 GB, DDR4\n Ổ cứng : SSD, 512GB\n Màn hình : 15.6 inchs \n Card màn hình : GTX1050, Card rời\n Cổng kết nối : LAN : 10/100/1000 Mbps, WIFI : 802.11 ac\n Hệ điều hành : windows 10 home single language\n Trọng lượng : 2.2 kg\n Kích thước : 36 x 27.5 x 2.58 cm (W x D x H)\n Xuất xứ : Trung Quốc \n Năm sản xuất : 2019",
+    img1: "\image\asus-strix-g531-den-1.png",
+    img2: "\image\asus-strix-g531-den-2.png",
+    img3: "\image\asus-strix-g531-den-3.png",
+    img4: "\image\asus-strix-g531-den-4.png"
+};
+
+var Laptop = [DellXPS15, DellXPS13, DellXPS139, DellG3, DellInspiron, AsusZenDuo, AsusStrixG531GT, AsusZenUX, AsusZephyrus, AsusStrixG531GD];
