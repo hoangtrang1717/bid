5d993f4db7133c28ed40de0d41e3885b7fa82912
Steven Black <stevenblack1717@gmail.com>
2020-01-01 09:38:10 +0700

    Change port listen

--- app.js
+++ app.js
@@ -2,23 +2,17 @@ const http = require('http')
 const express = require('express');
 const exphbs  = require('express-handlebars');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"})
-  response.end("Hello\n")
-}).listen(process.env.PORT)
 const app = express();
-
 app.engine('hbs', exphbs({
   defaultLayout: 'main.hbs'
 }));
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Listening on port " + port);
+
 app.get('/', function(req, res) {
     res.render('home');
-})
-
-const PORT = 3000;
-
-app.listen(PORT, function() {
-    console.log(`Server is running at http://localhost:${PORT}`);
 })
